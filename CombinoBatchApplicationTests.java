b7238130b799f2bdaf43f25bfc495cc046d19db6
sar25rdesai <98273843+sar25rdesai@users.noreply.github.com>
2022-01-24 14:11:03 -0500

    Test Commit
    Add my test commit .

---
+++ CombinoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.combino.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CombinoBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
